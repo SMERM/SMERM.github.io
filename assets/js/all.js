c77d99ea228ced3e540d4bbf245da10e2460c572
grammaton <grammaton@me.com>
2015-10-30 18:47:23 +0100

    fixes

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,11 +2,3 @@
 ---
 
 {% include js/main.js %}
-
-// GITTER SIDECAR
-<script>
-  ((window.gitter = {}).chat = {}).options = {
-    room: 'SMERM/smerm.github.io'
-  };
-</script>
-<script src="https://sidecar.gitter.im/dist/sidecar.v1.js" async defer></script>
