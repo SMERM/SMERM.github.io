804c76fb48db69660c12975a1a447cbe96a2d2c1
grammaton <grammaton@me.com>
2015-11-10 18:12:55 +0100

    add gitter sidecar

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,3 +2,10 @@
 ---
 
 {% include js/main.js %}
+
+<script>
+  ((window.gitter = {}).chat = {}).options = {
+    room: 'SMERM/smerm.github.io'
+  };
+</script>
+<script src="https://sidecar.gitter.im/dist/sidecar.v1.js" async defer></script>
